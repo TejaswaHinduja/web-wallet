2bc92f88d5c484bfd624b536b05a17b905f89c5f
Tejaswa <tejaswahinduja@gmail.com>
2026-01-28 23:42:20 +0530

    done , time to write doc

--- components/ui/lamp.tsx
+++ components/ui/lamp.tsx
@@ -32,14 +32,14 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        "relative flex min-h-screen flex-col items-center justify-start overflow-hidden bg-gray-50 w-full z-0 pt-32",
+        "relative flex min-h-screen flex-col items-center justify-start overflow-hidden bg-gray-50 w-full z-0",
         className
       )}
     >
-      <div className="relative flex w-full flex-1 scale-y-100 items-start justify-center isolate z-0 ">
+      <div className="relative flex w-full h-64 items-center justify-center isolate z-0 mt-8">
         <motion.div
-          initial={{ opacity: 0.3, width: "15rem" }}
-          whileInView={{ opacity: 0.6, width: "25rem" }}
+          initial={{ opacity: 0.3, width: "20rem" }}
+          whileInView={{ opacity: 0.6, width: "40rem" }}
           transition={{
             delay: 0.2,
             duration: 0.6,
@@ -48,14 +48,14 @@ export const LampContainer = ({
           style={{
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
-          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[25rem] bg-gradient-conic from-blue-300 via-transparent to-transparent [--conic-position:from_70deg_at_center_top]"
+          className="absolute inset-auto right-1/2 h-64 overflow-visible w-[40rem] bg-gradient-conic from-blue-300 via-transparent to-transparent [--conic-position:from_70deg_at_center_top]"
         >
-          <div className="absolute w-[100%] left-0 bg-gray-50 h-32 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div className="absolute w-40 h-[100%] left-0 bg-gray-50 bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+          <div className="absolute w-[100%] left-0 bg-gray-50 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-48 h-[100%] left-0 bg-gray-50 bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
         </motion.div>
         <motion.div
-          initial={{ opacity: 0.3, width: "15rem" }}
-          whileInView={{ opacity: 0.6, width: "25rem" }}
+          initial={{ opacity: 0.3, width: "20rem" }}
+          whileInView={{ opacity: 0.6, width: "40rem" }}
           transition={{
             delay: 0.2,
             duration: 0.6,
@@ -64,38 +64,38 @@ export const LampContainer = ({
           style={{
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
-          className="absolute inset-auto left-1/2 h-56 w-[25rem] bg-gradient-conic from-transparent via-transparent to-blue-300 [--conic-position:from_290deg_at_center_top]"
+          className="absolute inset-auto left-1/2 h-64 w-[40rem] bg-gradient-conic from-transparent via-transparent to-blue-300 [--conic-position:from_290deg_at_center_top]"
         >
-          <div className="absolute w-40 h-[100%] right-0 bg-gray-50 bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-          <div className="absolute w-[100%] right-0 bg-gray-50 h-32 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-48 h-[100%] right-0 bg-gray-50 bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+          <div className="absolute w-[100%] right-0 bg-gray-50 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </motion.div>
-        <div className="absolute top-1/2 h-32 w-full translate-y-8 scale-x-150 bg-gray-50 blur-2xl"></div>
-        <div className="absolute inset-auto z-50 h-28 w-[24rem] -translate-y-1/2 rounded-full bg-blue-200 opacity-30 blur-3xl"></div>
+        <div className="absolute top-1/2 h-40 w-full translate-y-8 scale-x-150 bg-gray-50 blur-2xl"></div>
+        <div className="absolute inset-auto z-50 h-32 w-[36rem] -translate-y-1/2 rounded-full bg-blue-200 opacity-30 blur-3xl"></div>
         <motion.div
-          initial={{ width: "6rem" }}
-          whileInView={{ width: "12rem" }}
+          initial={{ width: "8rem" }}
+          whileInView={{ width: "16rem" }}
           transition={{
             delay: 0.2,
             duration: 0.6,
             ease: "easeInOut",
           }}
-          className="absolute inset-auto z-30 h-28 w-48 -translate-y-[4rem] rounded-full bg-blue-200 opacity-40 blur-2xl"
+          className="absolute inset-auto z-30 h-32 w-64 -translate-y-[4rem] rounded-full bg-blue-200 opacity-40 blur-2xl"
         ></motion.div>
         <motion.div
-          initial={{ width: "10rem" }}
-          whileInView={{ width: "20rem" }}
+          initial={{ width: "15rem" }}
+          whileInView={{ width: "30rem" }}
           transition={{
             delay: 0.2,
             duration: 0.6,
             ease: "easeInOut",
           }}
-          className="absolute inset-auto z-50 h-0.5 w-[20rem] -translate-y-[5rem] bg-blue-300 opacity-70"
+          className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[5rem] bg-blue-300 opacity-70"
         ></motion.div>
 
-        <div className="absolute inset-auto z-40 h-32 w-full -translate-y-[10rem] bg-gray-50"></div>
+        <div className="absolute inset-auto z-40 h-40 w-full -translate-y-[12rem] bg-gray-50"></div>
       </div>
 
-      <div className="relative z-50 flex flex-col items-center px-5 w-full max-w-6xl">
+      <div className="relative z-50 flex flex-col items-center px-5 w-full max-w-6xl -mt-42">
         {children}
       </div>
     </div>
